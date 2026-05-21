46552ba2de114ac8f8bbc911a0df904cdaffdd3c
Dylan Goncalves <dylanphx93@gmail.com>
2026-05-21 12:34:27 +0200

    Update queries.js

--- query/queries.js
+++ query/queries.js
@@ -1,5 +1,6 @@
 // 1. Mostrar tots els documents en la col·lecció Restaurants.
-db.restaurants.find()
+db.restaurants.find({ _id: 0, })
 // 2. Mostrar el restaurant_id i name per tots els documents en la col·lecció Restaurants.
-db.restaurants.find({},{ restaurant_id: 1, name: 1 })
-
+db.restaurants.find({},{ _id: 0, restaurant_id: 1, name: 1 })
+// 3. Mostrar el restaurant_id, name, borough i cuisine.
+db.restaurants.find({},{ _id: 0, restaurant_id: 1, name: 1, borough: 1, cuisine: 1 })
