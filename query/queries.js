c36a196c84ae8e8a2cf1650a584d9990c3b35403
Dylan Goncalves <dylanphx93@gmail.com>
2026-05-21 19:04:04 +0200

    Add queries to find restaurants by score

--- query/queries.js
+++ query/queries.js
@@ -12,6 +12,9 @@ db.restaurants.find({ borough: "Bronx" },{ _id: 0 })
 db.restaurants.find({ borough: "Bronx" },{ _id: 0 }).limit(5)
 // 7. Mostrar el pròxims 5 restaurants després de saltar els primers 5 del Bronx.
 db.restaurants.find({ borough: "Bronx" },{ _id: 0 }).skip(5).limit(5)
-
+// 8. Trobar els restaurants amb un score de més de 90.
+db.restaurants.find({ "grades.score": { $gt: 90 }}, { _id: 0 })
+// 9. Trobar els restaurants amb un score de més de 80 però menys que 100.
+db.restaurants.find({ "grades.score": { $gt: 80 },{ $lt: 100 }},{ _id: 0 })
 
 
