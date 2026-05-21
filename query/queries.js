aed60b9334e124d892b34910ef53da2be4bc6c39
Dylan Goncalves <dylanphx93@gmail.com>
2026-05-21 12:31:15 +0200

    Add query to display restaurant_id and name

--- query/queries.js
+++ query/queries.js
@@ -1,3 +1,5 @@
 // 1. Mostrar tots els documents en la col·lecció Restaurants.
 db.restaurants.find()
+// 2. Mostrar el restaurant_id i name per tots els documents en la col·lecció Restaurants.
+db.restaurants.find({},{ restaurant_id: 1, name: 1 })
 
